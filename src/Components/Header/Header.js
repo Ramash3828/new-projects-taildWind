85a5aa7ef8cc0b8dccae623e0bbbb0845016f688
Ramash3828 <ramash3828@gmail.com>
2022-03-29 09:10:46 +0600

    menubar responsive

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,30 +1,42 @@
-import React from "react";
-
+import React, { useState } from "react";
+import { MenuIcon, XIcon } from "@heroicons/react/solid";
 const Header = () => {
+    const [isShow, setIsShow] = useState(false);
+
     return (
-        <div className="container mx-auto px-20">
-            <nav className="md:flex justify-between">
-                <div className="logo">
+        <div className="container bg-slate-600 transition ease-in duration-1000">
+            <nav className="md:flex justify-between items-center py-4 relative">
+                <div className="logo text-left">
                     <h2 className="text-2xl py-2 font-bold ">LOGO</h2>
                 </div>
                 <navbar>
-                    <ul className="md:flex gap-3">
-                        <li>
+                    <ul
+                        className={`md:flex gap-3 ${
+                            isShow ? "block" : "hidden"
+                        }`}
+                    >
+                        <li className="px-2 py-1 m-2 bg-slate-900 text-white border border-slate-900 hover:bg-transparent transition-all  rounded-md">
                             <a href="/">Home</a>
                         </li>
-                        <li>
+                        <li className="px-2 py-1 m-2 bg-slate-900 text-white border border-slate-900 hover:bg-transparent transition-all  rounded-md">
                             <a href="/">Product</a>
                         </li>
-                        <li>
+                        <li className="px-2 py-1 m-2 bg-slate-900 text-white border border-slate-900 hover:bg-transparent transition-all  rounded-md">
                             <a href="/">Contact</a>
                         </li>
-                        <li>
+                        <li className="px-2 py-1 m-2 bg-slate-900 text-white border border-slate-900 hover:bg-transparent transition-all  rounded-md">
                             <a href="/">About</a>
                         </li>
-                        <li>
+                        <li className="px-2 py-1 m-2 bg-slate-900 text-white border border-slate-900 hover:bg-transparent transition-all  rounded-md">
                             <a href="/">Login</a>
                         </li>
                     </ul>
+                    <div
+                        onClick={() => setIsShow(!isShow)}
+                        className=" w-6 h-6 md:hidden cursor-pointer absolute right-5 top-5 "
+                    >
+                        {isShow ? <XIcon></XIcon> : <MenuIcon></MenuIcon>}
+                    </div>
                 </navbar>
             </nav>
         </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,16 @@
 module.exports = {
     content: ["./src/**/*.{js,jsx,ts,tsx}"],
     theme: {
-        extend: {},
+        extend: {
+            container: {
+                sm: "576px",
+                md: "768px",
+                lg: "992px",
+                xl: "1200px",
+                center: true,
+                padding: "16px",
+            },
+        },
     },
     plugins: [],
 };
